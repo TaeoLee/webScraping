f513e6b4546099986e108c81f8a09d19ee6534f0
Taeo Lee <g1238245@pukyong.ac.kr>
2025-03-24 06:14:13 +0000

    quit scraping

--- webScrapingNomal.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-
-url= "https://kr.investing.com/indices/us-spx-500-futures?cid=1175153"
-
-page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-print(html)
